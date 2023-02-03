edf136dbdcb11f1be497a740409970280ed1e360
Lynix <lynix@lynix.ca>
2023-02-03 16:25:04 +0000

    Get API Stuff

--- pages/api/hub/page_editor/index.ts
+++ pages/api/hub/page_editor/index.ts
@@ -6,14 +6,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { id } = req.query;
     const { db } = await connectToDatabase();
 
-    let page_data: any
-
-    try {
-        page_data = await db.collection("pages").findOne({_id: new ObjectId(id?.toString())});
-    } catch {
-        page_data = {}
+    // Check if POST or GET
+    if (req.method === 'POST') {
+        const { id, str_id, title, content, icon } = req.query;
+        // Wait 5 seconds
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        // Send Data
+        res.status(200).json({ success: false });
+        return;
     }
-    const page_list = await db.collection("pages").find({}).project({ str_id: 1, page_icon: 1, title: 1 }).toArray();
 
-    res.status(200).json({selected_page: page_data, all_pages: page_list})
+    if (req.method === 'GET') {
+        let page_data: any
+
+        try {
+            page_data = await db.collection("pages").findOne({_id: new ObjectId(id?.toString())});
+        } catch {
+            page_data = {}
+        }
+        const page_list = await db.collection("pages").find({}).project({ str_id: 1, page_icon: 1, title: 1 }).toArray();
+    
+        res.status(200).json({selected_page: page_data, all_pages: page_list})
+    }
 }
